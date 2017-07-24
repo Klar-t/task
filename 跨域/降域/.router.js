285a965697c7bf4765bc06db81d5c8c218e2cc83
Liqiaomiao <1049259456@qq.com>
2017-07-24 13:29:28 +0800

    增加跨域方法

---
+++ 跨域/降域/.router.js
@@ -0,0 +1,8 @@
+function setRouter(app){ 
+ var router = app; 
+
+/**
+ * Created by Liqiaomiao on 2017/7/24.
+ */
+}
+ module.exports.setRouter = setRouter
---
+++ 跨域/降域/router.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Liqiaomiao on 2017/7/24.
+ */
